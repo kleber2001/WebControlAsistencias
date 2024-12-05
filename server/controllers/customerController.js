24241bef174d6e3cd522056392a0e3e864d7eb55
kleber2001 <kgnunez@espol.edu.ec>
2024-12-04 20:48:10 -0500

    editar registrar

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,56 +1,63 @@
 const User = require('../models/Customer');
 const mongoose = require('mongoose');
 
-//GET HOME 
-
-exports.homepage = async(req, res)=>{
-
-        const locals = {
-            title : 'Asistencia',
-            description : 'Control de asistencia del LST'
-        }
+// GET HOME 
+exports.homepage = async (req, res) => {
+    const locals = {
+        title: 'Asistencia',
+        description: 'Control de asistencia del LST'
+    };
     res.render('index', locals);
-}
+};
 
 // GET NEW USER
-
-exports.addCustomer = async(req, res)=>{
-
+exports.addCustomer = async (req, res) => {
     const locals = {
-        
-        title : 'Nuevo registro',
-        description : 'Ingreso de nuevo usuario'
-        
-    }
+        title: 'Nuevo registro',
+        description: 'Ingreso de nuevo usuario',
+        message: req.query.message || null,  
+        messageType: req.query.messageType || 'info'  
+    };
     res.render('customer/add', locals);
-}
+};
 
-// GET ADD USER
-
-exports.postCustomer = async(req, res)=>{
-    
-    console.log(req.body);
-
-    const paralelo = req.body;
-
-    const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        identificacion: req.body.identificacion,
-        lector: req.body.lector,
-        materia: req.body.materia,
-        paralelo: req.body.paralelo,
-        role: req.body.role
-
-    });
-
+// POST ADD USER
+exports.postCustomer = async (req, res) => {
+    const ident = req.body.identificacion;
 
     try {
-    
-        await User.create(newUser);
-        res.redirect('/add');
+        const existUser = await User.findOne({ identificacion: ident });
+
+        if (existUser) {
+            return res.render('customer/add', {
+                title: 'Nuevo registro',
+                description: 'Ingreso de nuevo usuario',
+                message: 'Error: Este usuario ya se encuentra registrado.',
+                messageType: 'danger'
+            });
+        }
+
+        const newUser = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            identificacion: req.body.identificacion,
+            lector: req.body.lector,
+            materia: req.body.materia,
+            paralelo: req.body.paralelo,
+            role: req.body.role
+        });
+
+        await newUser.save();
+
+        // Si todo es correcto, mostramos el mensaje de éxito
+        return res.render('customer/add', {
+            title: 'Nuevo registro',
+            description: 'Ingreso de nuevo usuario',
+            message: 'Registro exitoso!',
+            messageType: 'success'
+        });
+
     } catch (error) {
         console.log(error);
     }
-
-}
+};
